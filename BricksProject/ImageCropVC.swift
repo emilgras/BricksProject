5cd7a762935e6f18cacdaf91eb273e2e237029c1
Emil Gräs <emilgras@Emils-MacBook-Pro.local>
2017-05-21 18:18:34 +0200

    work in progress

--- BricksProject/ImageCropVC.swift
+++ BricksProject/ImageCropVC.swift
@@ -9,22 +9,14 @@
 import UIKit
 
 class ImageCropVC: UIViewController {
-
+    
     var image: UIImage?
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageViewEqualHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewEqualWidthConstraint: NSLayoutConstraint!
     
     @IBAction func cropButtonTapped(_ sender: Any) {
-        let scale:CGFloat = 1/scrollView.zoomScale
-        let x:CGFloat = scrollView.contentOffset.x * scale
-        let y:CGFloat = scrollView.contentOffset.y * scale
-        let width:CGFloat = scrollView.frame.size.width * scale
-        let height:CGFloat = scrollView.frame.size.height * scale
-        let croppedCGImage = imageView.image?.cgImage?.cropping(to: CGRect(x: x, y: y, width: width, height: height))
-        let croppedImage = UIImage(cgImage: croppedCGImage!)
-        setImageToCrop(image: croppedImage)
+        let croppedImage = cropImage(image: image!)
+        setCroppedImage(image: croppedImage)
     }
     
     @IBAction func dismissTapped(_ sender: Any) {
@@ -33,8 +25,8 @@ class ImageCropVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollView.delegate = self
-        setImageToCrop(image: image!)
+        setupScrollView()
+        addImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,10 +34,34 @@ class ImageCropVC: UIViewController {
     }
     
     // MARK: - Helper Methods
-    func setImageToCrop(image:UIImage) {
+    fileprivate func setupScrollView() {
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 2.0
+        scrollView.zoomScale = 1.0
+    }
+    
+    fileprivate func addImage() {
+        guard let image = image else { return }
         imageView.image = image
-        imageViewEqualWidthConstraint.constant = image.size.width
-        imageViewEqualHeightConstraint.constant = image.size.height
+    }
+    
+    fileprivate func cropImage(image: UIImage) -> UIImage {
+        let scale:CGFloat = 1/scrollView.zoomScale
+        print("scale: \(scale)")
+        let x:CGFloat = scrollView.contentOffset.x * scale
+        let y:CGFloat = scrollView.contentOffset.y * scale
+        let width:CGFloat = scrollView.frame.size.width * scale
+        let height:CGFloat = scrollView.frame.size.height * scale
+        let croppedCGImage = imageView.image?.cgImage?.cropping(to: CGRect(x: x, y: y, width: width, height: height))
+        return UIImage(cgImage: croppedCGImage!)
+    }
+    
+    fileprivate func setCroppedImage(image:UIImage) {
+        imageView.image = image
+//        imageViewEqualWidthConstraint.constant = image.size.width
+//        imageViewEqualHeightConstraint.constant = image.size.height
+
         let scaleHeight = scrollView.frame.size.width/image.size.width
         let scaleWidth = scrollView.frame.size.height/image.size.height
         scrollView.minimumZoomScale = max(scaleWidth, scaleHeight)
@@ -56,5 +72,14 @@ class ImageCropVC: UIViewController {
 extension ImageCropVC: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let scale = imageView.frame.height/scrollView.frame.height
+        print(scale)
     }
 }
