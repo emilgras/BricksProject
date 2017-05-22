5a035cec1f814e91c2c43e7df05d4616df8602cf
Emil Gräs <emilgras@Emils-MacBook-Pro.local>
2017-05-22 16:00:17 +0200

    minor changes

--- BricksProject/PaymentViewCardNumberField.swift
+++ BricksProject/PaymentViewCardNumberField.swift
@@ -9,25 +9,45 @@
 import UIKit
 
 class PaymentViewCardNumberField: UITextField, UITextFieldDelegate {
-    var cardNumber: String?
+    var value: String = String()
+    fileprivate let limit: Int = 8
+    fileprivate let groupSize: Int = 4
+    fileprivate let spacingSize: Int = 2
+    fileprivate let delimitter = "  "
     
-//    internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        guard var text = textField.text else { return false }
-//        let newLength = text.characters.count + string.characters.count - range.length
-//        if !string.isEmpty {
-//            cardNumber += string
-//        } else {
-//            let newEndIndex = cardNumber.index(cardNumber.endIndex, offsetBy: -1)
-//            cardNumber = cardNumber.substring(to: newEndIndex)
-//        }
-//        
-//        if (newLength == 5 || newLength == 10+1 || newLength == 15+2) && !string.isEmpty {
-//            cardNumberTextField.text = text + "  "
-//        } else if (newLength == 5 || newLength == 10+1 || newLength == 15+2 || newLength == 20+3) && string.isEmpty {
-//            let newEndIndex = cardNumberTextField.text!.index(cardNumberTextField.text!.endIndex, offsetBy: -2)
-//            let newText = cardNumberTextField.text!.substring(to: newEndIndex)
-//            cardNumberTextField.text = newText
-//        }
-//        return newLength <= (cardNumberLimit + cardNumberSpacing)
-//    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    fileprivate func commonInit() {
+        
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard var text = textField.text else { return false }
+        let newLength = text.characters.count + string.characters.count - range.length
+        let addingValue = !string.isEmpty
+        
+        if addingValue {
+            value += string
+        } else {
+            let newEndIndex = value.index(value.endIndex, offsetBy: -1)
+            value = value.substring(to: newEndIndex)
+        }
+        
+        if (newLength == groupSize + 1 || newLength == (2 * groupSize) + spacingSize + 1 || newLength == (3 * groupSize) + (2 * spacingSize) + 1) && addingValue {
+            text = text + delimitter
+        } else if (newLength == groupSize + 1 || newLength == (2 * groupSize) + spacingSize + 1 || newLength == (3 * groupSize) + (2 * spacingSize) + 1) && !addingValue {
+            let newEndIndex = text.index(text.endIndex, offsetBy: -delimitter.characters.count)
+            let newText = text.substring(to: newEndIndex)
+            text = newText
+        }
+        return value.characters.count <= limit
+    } 
 }
--- BricksProject/PaymentViewTextField.swift
+++ BricksProject/PaymentViewTextField.swift
@@ -2,15 +2,11 @@
 //  PaymentViewTextField.swift
 //  BricksProject
 //
-//  Created by Emil Gräs on 21/05/2017.
+//  Created by Emil Gräs on 22/05/2017.
 //  Copyright © 2017 Emil Gräs. All rights reserved.
 //
 
 import UIKit
-
-protocol PaymentViewTextFieldDelegate {
-    
-}
 
 class PaymentViewTextField: UITextField {
     
